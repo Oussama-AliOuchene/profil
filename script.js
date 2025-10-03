9aea4deade952e5e47ba6f8f13c37fa004f9f4b4
Oussama-AliOuchene <o_aliouchene@estin.dz>
2025-10-03 19:41:46 +0100

    first2

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ function openLightbox(src) {
 function closeLightbox() {
   document.getElementById('lightbox').style.display = 'none';
 }
+document.addEventListener("DOMContentLoaded", function() {
+  const btn = document.getElementById('download-stages-pdf');
+  if (btn) {
+    btn.addEventListener('click', function() {
+      const stagesSection = document.getElementById('stages');
+      html2canvas(stagesSection).then(function(canvas) {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new window.jspdf.jsPDF();
+        const imgProps= pdf.getImageProperties(imgData);
+        const pdfWidth = pdf.internal.pageSize.getWidth();
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+        pdf.save("stages.pdf");
+      });
+    });
+  }
+});
